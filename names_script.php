22a6d6c9b70a1720e84f70cc1569f081c2b04036
007suraj <suraj.chaunal@yahoo.com>
2015-11-23 22:01:06 +0530

    Create names_script.php

---
+++ names_script.php
@@ -0,0 +1,12 @@
+<html> 
+<head>
+<link type="text/css" rel="stylesheet" href="script2_style.css">
+</head> 
+<body> 
+<?php 
+$host="localhost";
+$user="root";
+$password="";
+
+$rng=$_POST["output"];
+$year=$_POST["year"];
