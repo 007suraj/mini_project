548525dd0722676eef6fce831e0023e56f8bdc66
007suraj <suraj.chaunal@yahoo.com>
2015-11-25 11:21:08 +0530

    Create search.php

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+
+$con=mysqli_connect($host,$user,$password,'names')or die("error connecting to the database");
+ 
+ $term=$_GET["term"];
+ 
+ $qur="(SELECT name FROM female_2001 where name like '".$term."%') UNION  (SELECT name FROM male_2001 where name like '".$term."%')";
+
+$result=mysqli_query($con,$qur)or die("error querying database"); 
+$json=array();
+ 
+//echo($qur);
+$i=0;
+while($row=mysqli_fetch_array($result))
+{
+if($i<10)
+{
+$json[]=array(
+                    'value'=> $row[0]
+                        );
+$i=$i+1;
+}
+}
+
+ 
+ 
+ echo json_encode($json);
+ 
+?>
